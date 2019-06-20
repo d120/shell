e2eb778642b3e88be7c74e4f661ccae9cb4cd58b
Johannes Lauinger <johannes@lauinger-it.de>
2019-06-20 21:07:53 +0200

    don't remove users from mailing list when creating a forward, log into a file

--- ldap/deluser.py
+++ ldap/deluser.py
@@ -14,6 +14,16 @@ IGNORE_UIDNUMBERS_ABOVE = 4999
 MAIL_DOMAINS = ["d120.de", "fachschaft.informatik.tu-darmstadt.de"]
 
 
+def print2(*string):
+    with open(logfile, "a+") as f:
+        print(*string)
+        print(*string, file=f)
+
+def input2(question):
+    with open(logfile, "a+") as f:
+        print(question, file=f)
+        return input(question)
+
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
 
@@ -76,31 +86,36 @@ def findLists(aliases):
 def safe_delete_directory(directory):
     try:
         subprocess.check_call(["sudo", "ls", directory], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-        print("Deleting {}...".format(directory))
+        print2("Deleting {}...".format(directory))
         subprocess.call(["sudo", "rm", "-rf", directory])
     except subprocess.CalledProcessError as e:
-        print("Skipping delete of {} because it does not exist".format(directory))
+        print2("Skipping delete of {} because it does not exist".format(directory))
 
 
 def main(args):
-    print("Please authenticate using sudo...")
+    uid = arg_data.uid[0]
+    subprocess.call(["mkdir", "-p", "/mnt/media/fss/userdel-logs"])
+    global logfile
+    logfile = "/mnt/media/fss/userdel-logs/userdel-{}-log.txt".format(uid)
+    print2("Executing userdel {} at {}".format(uid, datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+    print2("Logging to {}".format(logfile))
+    print2("Please authenticate using sudo...")
     subprocess.call(["sudo", "true"])
     global c
     c = init_ldap()
-    uid = arg_data.uid[0]
     # Get User DN
     c.search(search_base=LDAP_USER_SCOPE, search_filter='(uid='+uid+')', search_scope=SEARCH_SCOPE_WHOLE_SUBTREE, attributes=['mailAlias', 'uidNumber', 'displayName', 'homeDirectory', 'objectClass'])
     if len(c.response) == 0:
-        print("Error: User {} not found!".format(uid))
+        print2("Error: User {} not found!".format(uid))
         sys.exit()
     user_dn = c.response[0]['dn']
     attributes = c.response[0]['attributes']
 
     # Let admin verify DN
-    print()
+    print2()
     if not query_yes_no("Is this DN correct for the user to be deleted? {}".format(user_dn)):
         sys.exit()
-    print()
+    print2()
 
     # Get groups user is member of
     c.search(search_base=LDAP_GROUP_SCOPE, search_filter='(member='+user_dn+')', search_scope=SEARCH_SCOPE_WHOLE_SUBTREE, attributes=['cn'])
@@ -122,27 +137,27 @@ def main(args):
     # Get mailing lists user is a member of
     mailing_lists = findLists(addresses)
 
-    print('This script will delete the following user:')
-    print("username:\t {}".format(uid))
-    print("displayName:\t {}".format(attributes['displayName'][0]))
-    print("uid:\t\t {}".format(attributes['uidNumber'][0]))
-    print("home:\t\t {}".format(attributes['homeDirectory'][0]))
-    print("dn:\t\t {}".format(user_dn))
-    print()
+    print2('This script will delete the following user:')
+    print2("username:\t {}".format(uid))
+    print2("displayName:\t {}".format(attributes['displayName'][0]))
+    print2("uid:\t\t {}".format(attributes['uidNumber'][0]))
+    print2("home:\t\t {}".format(attributes['homeDirectory'][0]))
+    print2("dn:\t\t {}".format(user_dn))
+    print2()
     if addresses:
-        print("mail addresses:\t {}".format(", ".join(addresses)))
+        print2("mail addresses:\t {}".format(", ".join(addresses)))
     if groups:
-        print("groups:\t\t {}".format(", ".join(groups)))
+        print2("groups:\t\t {}".format(", ".join(groups)))
     if aliases:
-        print("aliases:\t {}".format(", ".join(["{} (total {})".format(alias, num) for (alias, target, num) in aliases])))
+        print2("aliases:\t {}".format(", ".join(["{} (total {})".format(alias, num) for (alias, target, num) in aliases])))
     if mailing_lists:
-        print("mailing lists:\t {}".format(", ".join([mailing_list for (mailing_list, mail) in mailing_lists])))
-    print()
+        print2("mailing lists:\t {}".format(", ".join([mailing_list for (mailing_list, mail) in mailing_lists])))
+    print2()
 
     # Ask whether to create a mail forward
     if addresses:
-        forwardAddress = input("This user has some email addresses. Enter a new address to create a forwarding. Empty address will not create any: ")
-        print()
+        forwardAddress = input2("This user has some email addresses. Enter a new address to create a forwarding and not remove them from mailing lists. Empty address will not create any: ")
+        print2()
 
     # For aliases that would become empty, confirm that they will be deleted completely
     aliases_to_remove_user_from = []
@@ -155,26 +170,26 @@ def main(args):
                 if query_yes_no("Alias {} would become empty, do you want to remove the alias object?".format(alias)):
                     aliases_to_delete_completely.append(alias)
                 else:
-                    print("Alias {} can not get empty. Aborting this script.".format(alias))
+                    print2("Alias {} can not get empty. Aborting this script.".format(alias))
                     sys.exit()
-        print()
+        print2()
 
-    print("Looking for files belonging to the user...")
+    print2("Looking for files belonging to the user...")
     files_to_adjust_filename = "/tmp/deluser-{}-files.txt".format(uid)
     with open(files_to_adjust_filename, "w") as f:
         subprocess.call(["sudo", "find", "/mnt/media", "-user",  uid], stdout=f)
     number_of_files = int(subprocess.check_output(["wc", "-l", files_to_adjust_filename]).decode().split(" ")[0])
     if number_of_files == 0:
-        print("There are no files belonging to this user in /mnt/media")
+        print2("There are no files belonging to this user in /mnt/media")
     else:
-        print("Found {} files belonging to {} in /mnt/media. Review them by looking at {}".format(number_of_files, uid, files_to_adjust_filename))
-    print()
+        print2("Found {} files belonging to {} in /mnt/media. Review them by looking at {}".format(number_of_files, uid, files_to_adjust_filename))
+    print2()
 
     # Let admin verify
-    if input("Proceed to delete? Confirm by typing the username to be deleted: ") != uid:
-        print("Aborting.")
+    if input2("Proceed to delete? Confirm by typing the username to be deleted: ") != uid:
+        print2("Aborting.")
         sys.exit()
-    print()
+    print2()
 
     # just as a safety precaution for the following rm commands, exit if uid is empty
     if not uid:
@@ -183,25 +198,25 @@ def main(args):
     # Remove user from groups
     if groups:
         for group in groups:
-            print("Removing {} from group {}...".format(uid, group))
+            print2("Removing {} from group {}...".format(uid, group))
             c.modify('cn='+group+','+LDAP_GROUP_SCOPE, {'member': (MODIFY_DELETE, ['uid='+uid+','+LDAP_USER_SCOPE])})
 
     # Remove user from aliases
     if aliases_to_remove_user_from:
         for (alias, target, num) in aliases_to_remove_user_from:
-            print("Removing {} from alias {}, {} members remaining...".format(target, alias, num-1))
+            print2("Removing {} from alias {}, {} members remaining...".format(target, alias, num-1))
             c.modify('mailAlias='+alias+','+LDAP_FORWARD_SCOPE, {'mailTarget': (MODIFY_DELETE, [target])})
 
     # Remove aliases that would get empty
     if aliases_to_delete_completely:
         for alias in aliases_to_delete_completely:
-            print("Removing alias {}...".format(alias))
+            print2("Removing alias {}...".format(alias))
             c.delete('mailAlias='+alias+','+LDAP_FORWARD_SCOPE)
 
     # Remove user from mailing lists
-    if mailing_lists:
+    if mailing_lists and not forwardAddress:
         for (mailing_list, mail) in mailing_lists:
-            print("Removing from mailing list {}...".format(mailing_list))
+            print2("Removing from mailing list {}...".format(mailing_list))
             subprocess.call(["sudo", "remove_members", mailing_list, mail])
 
     # Delete home directory
@@ -215,7 +230,7 @@ def main(args):
 
     # Create LDAP mailForward object for all mail addresses
     if addresses and forwardAddress:
-        print("Forwarding all email addresses to {}...".format(forwardAddress))
+        print2("Forwarding all email addresses to {}...".format(forwardAddress))
         firstAddress = addresses[0]
         attrs = {}
         attrs["mailAlias"] = addresses
@@ -223,20 +238,20 @@ def main(args):
         attrs["description"] = "Forwarding for former user {}, deleted at {}".format(uid, datetime.datetime.now().strftime("%Y-%m-%d"))
         c.add("mailAlias=" + firstAddress + "," + LDAP_FORWARD_SCOPE, ["d120mailForward"], attrs)
     else:
-        print("Skipping creation of any forward objects")
+        print2("Skipping creation of any forward objects")
 
     # Adjust file ownerships for any files that belong to this user
     if number_of_files > 0:
-        print("Changing owners to root for {} files belonging to {}...".format(number_of_files, uid))
+        print2("Changing owners to root for {} files belonging to {}...".format(number_of_files, uid))
         subprocess.call(["sudo", "xargs", "--arg-file", files_to_adjust_filename, "-d", "\\n", "chown", "root"])
     else:
-        print("Skipping ownership changes for any files")
+        print2("Skipping ownership changes for any files")
 
     # Delete LDAP user object
-    print("Deleting user object {}...".format(uid))
+    print2("Deleting user object {}...".format(uid))
     c.delete('uid='+uid+','+LDAP_USER_SCOPE)
 
-    print("\nMy job here is done. Have a super nice day :)")
+    print2("\nMy job here is done. Have a super nice day :)")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='d120-deluser')
@@ -245,5 +260,5 @@ if __name__ == '__main__':
     try:
         main(arg_data)
     except KeyboardInterrupt:
-        print("\nAborted by user!")
+        print2("\nAborted by user!")
         sys.exit()
